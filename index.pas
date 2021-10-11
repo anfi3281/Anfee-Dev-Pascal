31fab54d9eca80aac98f973f432771e036013263
anfi3281 <anfi3281@gmail.com>
2021-10-11 07:14:43 +0700

    Belajar if else

--- index.pas
+++ index.pas
@@ -1,26 +1,17 @@
 program index;
 uses crt;
 var
-no_plg, mtr_awl, mtr_akh, trf : longint;
-dsk, byr : real;
-nm_plg, alm_plg : string;
-
+angka : integer;
 begin
-    no_plg := 1;
-    nm_plg := 'Anfee';
-    alm_plg := 'Pekalongan';
-    mtr_awl := 10000;
-    mtr_akh := 10500;
-    trf := 1000 * (mtr_akh - mtr_awl);
-    dsk := 0.1 * trf;
-    byr := trf - dsk;
-
-    writeln('');
-    writeln('No Pelanggan : ', no_plg);
-    writeln('Nama Pelanggan : ', nm_plg);
-    writeln('Alamat Pelanggan : ', alm_plg);
-    writeln('Tarif : ', trf);
-    writeln('Diskon : ', dsk:10:0);
-    writeln('Bayar : ', byr:10:0);
-    readkey;
+    clrscr;
+    angka := 4;
+    if (angka > 5 ) then
+        begin
+            writeln('Variable "angka" lebih besar dari 5');
+        end
+    else
+        begin
+            Writeln('Variable "angka" lebih kecil dari 5')
+        end;
+    readln;
 end.
